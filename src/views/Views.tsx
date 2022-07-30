2e350365045afd61bfc84c1f09d3c103e0d4f28c
Matan Turgeman <matannturgeman@gmail.com>
2022-07-30 23:23:59 +0300

    add about us page

--- src/views/Views.tsx
+++ src/views/Views.tsx
@@ -1,21 +1,35 @@
 import * as React from "react";
 import Box from "../components/Box/Box";
 import EmptyBox from "../components/EmptyBox/EmptyBox";
+import AboutUs from "./AboutUs/AboutUs";
 import * as views from "../data/views.json";
+import { VIEWS } from "../constants/views.constants";
 
 function Views() {
   const renderView = (view) => {
-    const { label, value, isEmpty } = view;
+    const { value } = view;
+
+    switch (value) {
+      case VIEWS.aboutUs : {
+        return <AboutUs view={view} />
+      }
+      default:
+        return <h1>Example Text</h1>
+    }
+  };
+
+  const renderBox = (view) => {
+    const { value, isEmpty } = view;
     const BoxContainer = isEmpty ? EmptyBox : Box;
 
     return (
       <BoxContainer key={value} id={`page-${value}`}>
-        <h1>This is the {label} page</h1>
+        {renderView(view)}
       </BoxContainer>
     );
   };
 
-  return <>{Array.from(views).map(renderView)}</>;
+  return <>{Array.from(views).map(renderBox)}</>;
 }
 
 export default Views;
