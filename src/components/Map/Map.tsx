88ea4707c12568ae400eb32cea84a5809f2164fe
Matan Turgeman <matannturgeman@gmail.com>
2022-10-07 23:59:54 +0300

    Add map to about us

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,58 +1,62 @@
 import React, { Component } from "react";
-
+import PropTypes from "prop-types";
 import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 
 const AnyMap = Map as any;
-const AnyMarker = Marker as any;
 
 const customizeMap = {
   width: "100%",
   height: "100%",
 };
 
+const containerStyle = {
+    position: "relative",
+    width: "100%",
+    height: "100%",
+};
+
 class GoogleMapComponent extends Component {
-  constructor(props) {
+  private cord: { lng: any; lat: any };
+
+  constructor(props: any) {
     super(props);
-    this.state = {
-      cords: [
-        { latitude: 51.507351, longitude: -0.127758 },
-        { latitude: 31.046051, longitude: 34.851612 },
-        { latitude: 51.165691, longitude: 10.451526 },
-        { latitude: 52.215933, longitude: 19.134422 },
-        { latitude: 50.0874654, longitude: 14.4212535 },
-        { latitude: 7.5554942, longitude: 80.7137847 },
-      ],
-    };
+    this.cord = { lat: props.lat, lng: props.lng };
   }
 
   drawMarker = () => {
     // @ts-ignore
-    return this.state.cords.map((store, i) => {
-      return (
-        <Marker
-          key={i}
-          // @ts-ignore
-          id={i}
-          position={{
-            lat: store.latitude,
-            lng: store.longitude,
-          }}
-          onClick={() => console.log("Event Hanlder Called")}
-        />
-      );
-    });
+    const {
+      cord: { lat, lng },
+    } = this;
+    return (
+      <Marker
+        // @ts-ignore
+        id={"place"}
+        position={{
+          lat,
+          lng,
+        }}
+        onClick={() => console.log("Event Hanlder Called")}
+      />
+    );
   };
 
   render() {
+    // @ts-ignore
+    const {
+      cord: { lat, lng },
+    } = this;
     return (
       <AnyMap
         // @ts-ignore
+        containerStyle={containerStyle}
+        // @ts-ignore
         google={this.props.google}
         style={customizeMap}
-        zoom={6}
+        zoom={15}
         initialCenter={{
-          lat: 9.96233,
-          lng: 49.80404,
+          lat,
+          lng,
         }}
       >
         {this.drawMarker()}
@@ -61,6 +65,14 @@ class GoogleMapComponent extends Component {
   }
 }
 
-export default GoogleApiWrapper({
+const MapView = GoogleApiWrapper({
   apiKey: "AIzaSyBE7e89ahNbzJN1rqVTzzc4Ft6jy0r5aqk",
 })(GoogleMapComponent as any);
+
+const OuterMapView = (props) => <MapView {...props} />;
+
+OuterMapView.propTypes = {
+  lng: PropTypes.number,
+  lat: PropTypes.number,
+};
+export default OuterMapView;
