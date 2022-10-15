90995164d14cb51741a6d18052c85828108147a5
Matan Turgeman <matannturgeman@gmail.com>
2022-10-16 02:34:42 +0300

    Add close button, add mobile styles

--- src/views/Gallery/ImageModal.styles.ts
+++ src/views/Gallery/ImageModal.styles.ts
@@ -1,12 +1,28 @@
 export const styles = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  maxHeight: "70vh",
-  display: "flex",
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
+  box: {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    maxHeight: "80vh",
+    display: "flex",
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+    justifyContent: "center",
+    ["@media (max-width:768px)"]: { // eslint-disable-line no-useless-computed-key
+      width: "85vw",
+      padding: "10px",
+    },
+  },
+  closeIcon: {
+    position: "absolute",
+    right: 7,
+    top: 7,
+    cursor: "pointer",
+    ["@media (max-width:768px)"]: { // eslint-disable-line no-useless-computed-key
+      display: "none"
+    },
+  },
 };
