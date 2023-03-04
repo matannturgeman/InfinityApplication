291944bac6d82d14dae47935a09cefe2da873fa9
Matan Turgeman <matant@cymulate.com>
2023-03-04 16:21:31 +0200

    Move to env

--- src/emailkey.ts
+++
@@ -1,4 +0,0 @@
-export const SERVICE_ID = "service_a6f0z5i";
-export const PUBLIC_ID = "aKsqsvIJCEHHcUtOt";
-export const PRIVATE_ID = "DD--0D5S_pQb2BKa5QKgl";
-export const TEMPLATE_ID = "template_xvyq06g";
