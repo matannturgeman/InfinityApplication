2995b262a7aa2b49717b856f74cbcaa51a677ff9
Matan Turgeman <matant@cymulate.com>
2023-03-12 01:23:52 +0200

    refactor gallery code

--- src/views/Gallery/Gallery.tsx
+++ src/views/Gallery/Gallery.tsx
@@ -2,15 +2,54 @@ import React, { useState, useMemo } from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
-import {
-  GalleryProps,
-  Subject,
-  Image,
-} from "../../types/Views/gallery.types";
+import { GalleryProps, Subject, Image } from "../../types/Views/gallery.types";
 import TabPanel from "../../components/TabPanel/TabPanel";
 import ImageGroupContainer from "./ImageGroupContainer";
 import ImageModal from "./ImageModal";
 import { styles } from "./Gallery.styles";
+
+const RenderTabs = ({ selectedTab, handleChange, subjects }) => (
+  <Box sx={styles.TabsBox}>
+    <Tabs
+      centered
+      value={selectedTab}
+      onChange={handleChange}
+      aria-label="gallery tabs"
+    >
+      {subjects.map(
+        (subject: Subject, index: number): JSX.Element => (
+          <Tab value={index} label={subject.label} key={subject.name} />
+        )
+      )}
+    </Tabs>
+  </Box>
+);
+
+const RenderTabPanel = ({
+  subjects,
+  selectedTab,
+  onImageClick,
+}): JSX.Element => (
+  <>
+    {subjects.map(
+      (subject: Subject, index: number): JSX.Element => (
+        <TabPanel
+          selected={index === selectedTab}
+          index={index}
+          boxStyles={styles.tabPanelBox}
+          style={styles.tabPanel}
+          key={subject.name}
+          name="gallery"
+        >
+          <ImageGroupContainer
+            images={subject.images}
+            onImageClick={onImageClick}
+          />
+        </TabPanel>
+      )
+    )}
+  </>
+);
 
 const Gallery = (props: GalleryProps) => {
   const {
@@ -27,53 +66,22 @@ const Gallery = (props: GalleryProps) => {
     setSelectedImage(image);
   };
 
-  const renderTabs = () =>
-    subjects.map(
-      (subject: Subject, index: number): JSX.Element => (
-        <Tab value={index} label={subject.label} key={subject.name} />
-      )
-    );
-
-  const tabs = useMemo(renderTabs, [subjects]);
-
-  const renderTabPanel = () =>
-    subjects.map(
-      (subject: Subject, index: number): JSX.Element => (
-        <TabPanel
-          selected={index === selectedTab}
-          index={index}
-          boxStyles={styles.tabPanelBox}
-          style={styles.tabPanel}
-          key={subject.name}
-          name="gallery"
-        >
-          <ImageGroupContainer
-            images={subject.images}
-            onImageClick={onImageClick}
-          />
-        </TabPanel>
-      )
-    );
-
-  const tabsPanel = useMemo(renderTabPanel, [subjects, selectedTab]);
-
   const onCloseImageModal = () => {
     setSelectedImage(null);
   };
 
   return (
     <Box sx={styles.GalleryBox}>
-      <Box sx={styles.TabsBox}>
-        <Tabs
-          centered
-          value={selectedTab}
-          onChange={handleChange}
-          aria-label="gallery tabs"
-        >
-          {tabs}
-        </Tabs>
-      </Box>
-      {tabsPanel}
+      <RenderTabs
+        selectedTab={selectedTab}
+        handleChange={handleChange}
+        subjects={subjects}
+      />
+      <RenderTabPanel
+        subjects={subjects}
+        selectedTab={selectedTab}
+        onImageClick={onImageClick}
+      />
       <ImageModal selectedImage={selectedImage} onClose={onCloseImageModal} />
     </Box>
   );
